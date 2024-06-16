5e32dcda174c5644f54d09fe0b97f20469acb860
Jason Keung <jason.keungg@gmail.com>
2024-06-15 22:18:16 -0700

    Fix time to utc

--- script.js
+++ script.js
@@ -1,50 +1,5 @@
-// questions = {
-//     1: ["question1???", "answer1", "answer2"],
-//     2: ["question2???", "answer1", "answer2"],
-//     3: ["question3???", "answer1", "answer2"],
-//     4: ["question4???", "answer1", "answer2"],
-//     5: ["question5???", "answer1", "answer2"],
-//     6: ["question6???", "answer1", "answer2"],
-//     7: ["question7???", "answer1", "answer2"],
-// }
-
-// var questionNumber = 1
-// const maxQuestions = 7
-
-// document.getElementById('start-button').addEventListener('click', function() {
-//     document.getElementById('start-box').hidden = true;
-//     document.getElementById('quiz-box').hidden = false;
-//     renderQuiz();
-// });
-
-// document.getElementById('quiz-answer1').addEventListener('click', function() {
-//     incrementQuestion();
-// });
-
-// document.getElementById('quiz-answer2').addEventListener('click', function() {
-//     incrementQuestion();
-// });
-
-// const incrementQuestion = () => {
-//     questionNumber++;
-//     if (questionNumber > maxQuestions) {
-//         document.getElementById('quiz-box').hidden = true;
-//         document.getElementById('result-box').hidden = false;
-//     } else {
-//         renderQuiz();
-//     }
-// }
-
-// const renderQuiz = () => {
-//     document.getElementById("quiz-question").innerHTML=questions[questionNumber][0]; 
-//     document.getElementById("quiz-answer1").innerHTML=questions[questionNumber][1]; 
-//     document.getElementById("quiz-answer2").innerHTML=questions[questionNumber][2]; 
-// }
-
-
-
 // Set the date we're counting down to
-var countDownDate = new Date("Aug 3, 2024 05:00:00").getTime();
+var countDownDate = new Date(Date.UTC(2024, 7, 3, 9, 0, 0)).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
